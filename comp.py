22ee8382978a0218684567ebe0aba7ba3824854f
Jenny Gao <jgao658@gmail.com>
2018-04-25 18:17:54 -0400

    finished

---
+++ comp.py
@@ -0,0 +1,33 @@
+def checkPass(passw):
+    upper=["u" for x in range(0,len(passw)) if passw[x].isupper()==True]
+    lower=["l" for x in range(0,len(passw)) if passw[x].islower()==True]
+    num=["n" for x in range(0,len(passw)) if unicode(passw[x], 'utf-8').isnumeric()==True]
+    return len(upper)>=1 and len(lower)>=1 and len(num)>=1
+
+# print checkPass("FaB1")
+# print checkPass("a1")
+# print checkPass("FB1")
+# print checkPass("1")
+
+def strength(passw):
+    stren=1
+    upper=["u" for x in range(0,len(passw)) if passw[x].isupper()==True]
+    lower=["l" for x in range(0,len(passw)) if passw[x].islower()==True]
+    num=["n" for x in range(0,len(passw)) if unicode(passw[x], 'utf-8').isnumeric()==True]
+    nonal=["nonAl" for x in range(0,len(passw)) if passw[x]=="." or passw[x]=="?" or passw[x]=="!" or passw[x]=="&" or passw[x]=="#" or passw[x]=="," or passw[x]==";" or passw[x]==":" or passw[x]=="-" or passw[x]=="_" or passw[x]=="*"]
+    if len(upper)>0:
+        stren+=2
+    if len(lower)>0:
+        stren+=2
+    if len(num)>0:
+        stren+=2
+    if len(nonal)>0:
+        stren+=3
+    return stren
+
+# print strength("%a,B2")
+# print strength("%aB2")
+# print strength("%,B2")
+# print strength("%a2")
+# print strength("%abc")
+# print strength("%")
